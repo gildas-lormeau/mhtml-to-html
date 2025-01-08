851aac9ca1247aacb5f8aea9aeade9689ee4189a
Gildas <gildas.lormeau@gmail.com>
2025-01-08 19:56:33 +0100

    remove useless case

--- src/mod.js
+++ src/mod.js
@@ -32,13 +32,10 @@ function replaceReferences(media, base, asset) {
 }
 
 function convertAssetToDataURI(asset) {
-    switch (asset.encoding) {
-        case "quoted-printable":
-            return `data:${asset.mediaType};base64,${encodeBase64(asset.data)}`;
-        case "base64":
-            return `data:${asset.mediaType};base64,${asset.data}`;
-        default:
-            return `data:${asset.mediaType};base64,${encodeBase64(asset.data)}`;
+    if (asset.encoding === "base64") {
+        return `data:${asset.mediaType};base64,${asset.data}`;
+    } else {
+        return `data:${asset.mediaType};base64,${encodeBase64(asset.data)}`;
     }
 }
 
