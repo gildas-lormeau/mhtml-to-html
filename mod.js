25fb8fe13f0d26c0c08830df2d471883a2288ad1
Gildas <gildas.lormeau@gmail.com>
2025-01-10 01:34:59 +0100

    move dependencies

--- mod.js
+++ mod.js
@@ -1,43 +1,46 @@
 /* eslint-disable no-console */
-/* global globalThis, Deno, TextEncoder */
+/* global TextEncoder */
 
-import { expandGlob } from "jsr:@std/fs";
-import { isGlob } from "jsr:@std/path";
-import { parse, convert } from "./src/mod.js";
+let expandGlob, isGlob, parse, convert, DOMParser, args, readTextFile, writeTextFile, exit;
 
-async function main(config = { DOMParser: globalThis.DOMParser }) {
-    const positionals = Deno.args;
+function initDependencies(dependencies) {
+    ({ expandGlob, isGlob, parse, convert, DOMParser, args, readTextFile, writeTextFile, exit } = dependencies);
+}
+
+async function main() {
+    const config = { DOMParser };
+    const positionals = args;
     if (positionals.length < 1 || positionals.includes("-h") || positionals.includes("--help")) {
         console.log("Usage: mhtml-to-html <input> [output]");
         console.log(" input: The input MHTML file, wildcards are supported");
         console.log(" output: The output HTML file, if not specified, the input file will be used with the extension changed to .html");
         console.log("");
-        Deno.exit(1);
+        exit(1);
     } else {
         if (isGlob(positionals[0])) {
             for await (const file of expandGlob(positionals[0])) {
-                process(file.path, null, config);
+                await process(file.path, null, config);
             }
         } else {
-            process(positionals[0], positionals[1], config);
+            await process(positionals[0], positionals[1], config);
         }
     }
 }
 
-function process(input, output, config = { DOMParser: globalThis.DOMParser }) {
+async function process(input, output, config) {
     output = output || input.replace(/\.[^.]+$/, ".html");
     if (!output.endsWith(".html")) {
         output += ".html";
     }
     try {
-        const data = Deno.readTextFileSync(input);
+        const data = await readTextFile(input);
         const mhtml = parse(new TextEncoder().encode(data), config);
         const doc = convert(mhtml, config);
-        Deno.writeTextFileSync(output, doc.serialize());
+        await writeTextFile(output, doc.serialize());
     } catch (error) {
         console.error(`Error processing ${input}: ${error.message}`);
         console.error(error.stack);
     }
 }
 
-export { parse, convert, process, main };
+export { initDependencies, main, process, parse, convert };
