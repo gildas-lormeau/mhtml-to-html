da55600ed8b45a9880335673e6444a3d51d42fd3
Gildas <gildas.lormeau@gmail.com>
2025-01-10 01:36:21 +0100

    add options description

--- mod.js
+++ mod.js
@@ -11,9 +11,11 @@ async function main() {
     const config = { DOMParser };
     const positionals = args;
     if (positionals.length < 1 || positionals.includes("-h") || positionals.includes("--help")) {
-        console.log("Usage: mhtml-to-html <input> [output]");
+        console.log("Usage: mhtml-to-html <input> [output] [options]");
         console.log(" input: The input MHTML file, wildcards are supported");
         console.log(" output: The output HTML file, if not specified, the input file will be used with the extension changed to .html");
+        console.log("Options:");
+        console.log(" -h, --help: Show this help message");
         console.log("");
         exit(1);
     } else {
