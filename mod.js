435516c347f5f24a27ece7e55ad1a7eba42ca12f
Gildas <gildas.lormeau@gmail.com>
2025-01-10 01:37:19 +0100

    add line feed

--- mod.js
+++ mod.js
@@ -12,6 +12,7 @@ async function main() {
     const positionals = args;
     if (positionals.length < 1 || positionals.includes("-h") || positionals.includes("--help")) {
         console.log("Usage: mhtml-to-html <input> [output] [options]");
+        console.log("");
         console.log(" input: The input MHTML file, wildcards are supported");
         console.log(" output: The output HTML file, if not specified, the input file will be used with the extension changed to .html");
         console.log("Options:");
