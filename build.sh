1a24e8468ee51a9f939db3af56e10e916d720926
Gildas <gildas.lormeau@gmail.com>
2025-01-13 12:46:36 +0100

    fix filenames

--- build.sh
+++ build.sh
@@ -7,5 +7,5 @@ deno compile --allow-read --allow-write --ext=js --output=./dist/mhtml-to-html-a
 deno compile --allow-read --allow-write --ext=js --output=./dist/mhtml-to-html.exe --target=x86_64-pc-windows-msvc ./mhtml-to-html
 
 dev_id=$(security find-identity -p codesigning -v | grep "Apple Development" | awk '{print $2}')
-codesign -f -s $dev_id ./dist/mhtml-to-html-aarch64-apple-darwin
-codesign -f -s $dev_id ./dist/mhtml-to-html-x86_64-apple-darwin
+codesign -f -s $dev_id ./dist/mhtml-to-html-aarch64-apple
+codesign -f -s $dev_id ./dist/mhtml-to-html-x86_64-apple
