641db762d5e9a5b655bae6ecf9c0af3c09c1a69d
Gildas <gildas.lormeau@gmail.com>
2025-01-13 11:29:35 +0100

    fix permissions

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ deno compile --allow-read --allow-write --ext=js --output=./dist/mhtml-to-html-a
 deno compile --allow-read --allow-write --ext=js --output=./dist/mhtml-to-html-x86_64-apple-darwin --target=x86_64-apple-darwin ./mhtml-to-html
 deno compile --allow-read --allow-write --ext=js --output=./dist/mhtml-to-html-x86_64-linux --target=x86_64-unknown-linux-gnu ./mhtml-to-html
 deno compile --allow-read --allow-write --ext=js --output=./dist/mhtml-to-html-aarch64-linux --target=aarch64-unknown-linux-gnu ./mhtml-to-html
-deno compile --allow-all --ext=js --output=./dist/mhtml-to-html.exe --target=x86_64-pc-windows-msvc ./mhtml-to-html
+deno compile --allow-read --allow-write --ext=js --output=./dist/mhtml-to-html.exe --target=x86_64-pc-windows-msvc ./mhtml-to-html
 
 dev_id=$(security find-identity -p codesigning -v | grep "Apple Development" | awk '{print $2}')
 codesign -f -s $dev_id ./dist/mhtml-to-html-aarch64-apple-darwin
