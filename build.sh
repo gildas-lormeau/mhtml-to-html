70dff9393fad437551f639ffe60461cc6d3517df
Gildas <gildas.lormeau@gmail.com>
2025-01-19 00:46:36 +0100

    update script

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+mv node_modules node_modules.bak
+mv package.json package.json.bak
+mv package-lock.json package-lock.json.bak
+
 deno compile --allow-read --allow-write --allow-net --ext=js --output=./dist/mhtml-to-html-aarch64-apple --target=aarch64-apple-darwin ./mhtml-to-html
 deno compile --allow-read --allow-write --allow-net --ext=js --output=./dist/mhtml-to-html-x86_64-apple --target=x86_64-apple-darwin ./mhtml-to-html
 deno compile --allow-read --allow-write --allow-net --ext=js --output=./dist/mhtml-to-html-x86_64-linux --target=x86_64-unknown-linux-gnu ./mhtml-to-html
@@ -9,3 +13,7 @@ deno compile --allow-read --allow-write --allow-net --ext=js --output=./dist/mht
 dev_id=$(security find-identity -p codesigning -v | grep "Apple Development" | awk '{print $2}')
 codesign -f -s $dev_id ./dist/mhtml-to-html-aarch64-apple
 codesign -f -s $dev_id ./dist/mhtml-to-html-x86_64-apple
+
+mv node_modules.bak node_modules
+mv package.json.bak package.json
+mv package-lock.json.bak package-lock.json
